2bf7b54f9533ad5126e6d0a6aa38ac04641389c1
kurth4cker <kurth4cker@gmail.com>
2023-12-01 19:09:02 +0300

    sdie.c: Remove ecalloc()
    sdie.h: Remove unneeded macros

--- sdie.c
+++ sdie.c
@@ -24,13 +24,3 @@ die(const char *fmt, ...)
 
 	exit(1);
 }
-
-void *
-ecalloc(size_t nmemb, size_t size)
-{
-	void *p;
-
-	if (!(p = calloc(nmemb, size)))
-		die("calloc:");
-	return p;
-}
--- sdie.h
+++ sdie.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#ifndef SDIE_H
+#define SDIE_H
 
 void die(const char *fmt, ...);
-void *ecalloc(size_t nmemb, size_t size);
+
+#endif /* SDIE_H */
